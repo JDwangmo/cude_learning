c0a7cc4b9724fe70945334cdaba44f64ef7f2ad5
wangjundong <383287471@qq.com>
2018-06-12 21:31:27 +0800

    cuda入门例子

---
+++ build/CMakeFiles/untitled2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled2.dir/src/untitled2_generated_add.cu.o"
+  "CMakeFiles/untitled2.dir/main.cc.o"
+  "untitled2.pdb"
+  "untitled2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
