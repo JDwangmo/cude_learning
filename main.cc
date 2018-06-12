248189e2c1b6442cbc07701798a60b0659e3541a
wangjundong <383287471@qq.com>
2018-06-12 21:22:05 +0800

    init commit

---
+++ main.cc
@@ -0,0 +1,7 @@
+#include <iostream>
+#include "src/add.h"
+int main(){
+    add_cuda();
+    std::cout << "hello world!" << std::endl;
+    return 0;
+}
---
+++ src/add.h
@@ -0,0 +1,11 @@
+//
+// Created by wangjundong on 6/12/18.
+//
+
+#ifndef UNTITLED2_ADD_H
+#define UNTITLED2_ADD_H
+
+int add_cuda();
+
+
+#endif //UNTITLED2_ADD_H
